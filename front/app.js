b650107a440d4a2075b0be239ee27ee7ddbf7127
Youssef Bourais <ybourais@c2r4p4.1337.ma>
2024-10-26 18:47:56 +0100

    learning css

--- front/app.js
+++ front/app.js
@@ -1,48 +1,26 @@
+// app.js
 
-function homeView() 
+const templates = 
 {
-    return `
-        <h2>Home View</h2>
-        <p>Welcome to the Home Page!</p>
-    `;
-}
-
-function aboutView() 
-{
-    return `
-        <h2>About View</h2>
-        <p>Learn more about us on this page.</p>
-    `;
-}
-
-function contactView() 
-{
-    return `
-        <h2>Contact View</h2>
-        <p>Get in touch with us!</p>
-    `;
-}
-
-function showView(view) 
-{
-    const container = document.getElementById('ele');
-    
-    switch(view) {
-        case 'home':
-            container.innerHTML = homeView();
-            break;
-        case 'about':
-            container.innerHTML = aboutView();
-            break;
-        case 'contact':
-            container.innerHTML = contactView();
-            break;
-        default:
-            container.innerHTML = '<h2>404 - page not found</h2>';
-    }
-}
-
-window.onload = () => {
-    showView('home');
+  login: `
+    <div id="loginPage">
+      <h2>Login</h2>
+      <form id="loginForm">
+            <label for="username">Username:</label>
+                <input type="text" id="username" required>
+            <label for="password">Password:</label>
+            <input type="password" id="password" required>
+            <button type="submit">Login</button>
+      </form>
+    </div>
+  `,
+  home: `
+    <div id="homePage">
+      <h2>Home Page</h2>
+      <p>Welcome! You’re logged in.</p>
+      <button id="logoutButton">Logout</button>
+    </div>
+  `
 };
-
+console.log('app is connected!');
+document.write('<p>hello word</>p');
