d08a18e41efa22f32ad88bc76f239a24762932ec
mehdi <mehdi>
2024-12-23 06:24:36 +0100

    dddd

--- backend/api/chat/models.py
+++ backend/api/chat/models.py
@@ -25,31 +25,7 @@ class Message(models.Model):
         # Convert the DateTimeField to an ISO 8601 string
         return self.date.isoformat()
 
-    # def send_message(from_user, to_user, msg):
-    #     sender_message = Message(
-    #         sender=from_user,
-    #         recipient=to_user,
-    #         body=msg,
-    #         is_read=True)
-    #     sender_message.save()
 
-    #     recipient_message = Message(
-    #         sender=from_user,
-    #         recipient=to_user,
-    #         body=msg,)
-    #     recipient_message.save()
-    #     return sender_message
-    
-    # def get_messages(user):
-    #     messages = Message.objects.filter(recipient=user).values('recipient').annotate(last=Max('date')).order_by('-last')
-    #     users = []
-    #     for message in messages:
-    #         users.append({
-    #             'user': User.objects.get(pk=message['recipient']),
-    #             'last': message['last'],
-    #             'unread': Message.objects.filter(sender=user, recipient__pk=message['recipient'], is_read=False).count(),
-    #         })
-    #         return users
     
     def __str__(self):
         return f'{self.sender.username} -> {self.recipient.username}'
